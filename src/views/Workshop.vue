4954801afa498333385a613587767bd9d442952c
Anton <antonoutkine@gmail.com>
2018-08-17 11:13:44 -0500

    Add workshop page

---
+++ src/views/Workshop.vue
@@ -0,0 +1,23 @@
+<template>
+  <Query
+    :query="require('@/graphql/Workshop.gql')"
+    :variables="{ name: $route.params.name }"
+  >
+    <template slot-scope="{ data: { workshop: { name, description } } }">
+      <h1>{{ name }}</h1>
+      <p>{{ description }}</p>
+    </template>
+  </Query>
+</template>
+
+<script>
+import Query from '@/components/Query'
+
+export default {
+  components: { Query },
+  name: 'workshop',
+  mounted () {
+    console.log(this.$route)
+  },
+}
+</script>
