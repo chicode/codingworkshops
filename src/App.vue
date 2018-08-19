2c605e7dc9a0183d44ebf0e951dce27d580cdb6f
Anton <antonoutkine@gmail.com>
2018-08-19 09:49:21 -0500

    Add status bar and remove styles left from boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <status-bar v-if="!$router.currentRoute.meta.noStatusBar" />
+    <router-view />
   </div>
 </template>
 <style lang="stylus">
 @import '~@/styles/index.styl'
 
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
+<script>
+import StatusBar from '@/components/StatusBar'
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+export default {
+  components: { StatusBar },
+}
+</script>
+
+<style lang="stylus">
+@import '~@/styles/index.styl'
 </style>
---
+++ src/components/StatusBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="root">
+    <div>
+      <router-link :to="{ name: 'home' }">Home</router-link>
+    </div>
+    <Query
+      :query="require('@/graphql/CurrentUser_minimal.gql')"
+    >
+      <template slot-scope="{ data: { username }}">
+        <div v-if="username">
+          <router-link :to="{ name: 'human', params: { username } }">{{ username }}</router-link>
+        </div>
+        <div v-else>
+          <router-link :to="{ name: 'enter' }">login</router-link>
+        </div>
+      </template>
+    </Query>
+  </div>
+</template>
+
+<script>
+import Query from '@/components/Query'
+
+export default {
+  components: { Query },
+}
+</script>
+
+<style scoped lang="stylus">
+.root {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
