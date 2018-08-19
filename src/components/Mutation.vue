b8a4971989a2284ba8d4d1c4954a6f64c8ef44cb
Anton <antonoutkine@gmail.com>
2018-08-19 12:05:06 -0500

    Actually add the mutation component

---
+++ src/components/Mutation.vue
@@ -0,0 +1,38 @@
+<template>
+  <ApolloMutation
+    :mutation="mutation"
+    :variables="variables"
+    @done="({ data }) => onDone(data)"
+  >
+    <template slot-scope="{ mutate, loading, error }">
+      <div v-if="loading">Loading...</div>
+
+      <div v-else-if="error">An error occured</div>
+
+      <slot
+        v-else
+        :mutate="mutate"
+      />
+    </template>
+  </ApolloMutation>
+</template>
+
+<script>
+export default {
+  name: 'Mutation',
+  props: {
+    mutation: {
+      type: Object,
+      required: true,
+    },
+    variables: {
+      type: Object,
+      default: () => ({}),
+    },
+    onDone: {
+      type: Function,
+      default: () => {},
+    },
+  },
+}
+</script>
