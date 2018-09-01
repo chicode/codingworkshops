d7af7f2bed62eb5591aecfe2712a3185aa386ad7
Anton <antonoutkine@gmail.com>
2018-08-31 20:06:25 -0500

    Add workshop page components

---
+++ src/components/LessonTile.vue
@@ -0,0 +1,31 @@
+<template>
+  <router-link
+    :to="{ name: 'lesson', params: { name: lesson.name } }"
+    class="lesson"
+    tag="li"
+  >
+    <h2>{{ lesson.name }}</h2>
+    <p>{{ lesson.description }}</p>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'LessonTile',
+  props: {
+    lesson: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped lang="stylus">
+@import '~@/styles/defs'
+
+.lesson {
+  tile('tile', palette.light-purple);
+  padding: 20px;
+}
+</style>
---
+++ src/components/LessonTiles.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="lessons">
+    <LessonTile
+      v-for="lesson in lessons"
+      :lesson="lesson"
+      :key="lesson.name"
+      class="lesson"
+    />
+  </ul>
+</template>
+
+<script>
+import LessonTile from '@/components/LessonTile'
+
+export default {
+  name: 'LessonTiles',
+  components: { LessonTile },
+  props: {
+    lessons: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped lang="stylus">
+@import '~@/styles/defs'
+.lessons {
+  .lesson {
+    width: 100%;
+  }
+}
+</style>
