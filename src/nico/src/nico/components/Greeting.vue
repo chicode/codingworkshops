a89ba999e45dd0b3d19385842733df20ce38fc30
Anton <antonoutkine@gmail.com>
2018-09-13 15:35:22 -0500

    Fix Nico span spacing issue

--- src/nico/src/nico/components/Greeting.vue
+++ src/nico/src/nico/components/Greeting.vue
@@ -4,18 +4,18 @@ div
     | Hi! Welcome to nico, the friendly online game engine. It may seem simple at first, but the reality is that this site contains all the tools you need to make really awesome games.
   p
     | Nico is divided into three main tabs. You are currently looking at the
-    span.accent-1 Game
+    span.accent-1 &nbsp;Game
     |  tab, which is where you will run your games. The
-    span.accent-2 Editor
+    span.accent-2 &nbsp;Editor
     |  tab allows you to edit your game's code. The
-    span.accent-3 Sprite
+    span.accent-3 &nbsp;Sprite
     |  tab allows you to draw all of your sprites.
   p
     | For starters, try drawing a sprite in the first grid square and getting it to display on the screen. Hint: the editor tab has a useful booklet that will tell you all about the different functions you can use to do stuff like that.
   p
     | After that, the possibilities really are endless - you can think of nico as that
-    span.accent-3 secret
-    span.accent-1 superpower
+    span.accent-3 &nbsp;secret
+    span.accent-1 &nbsp;superpower
     |  that you always thought you had. And if you ever decide to come back, you don't have to worry about anything - nico saves everything automatically (as long as you don't clear your cache or go into incognito mode).
 </template>
 
