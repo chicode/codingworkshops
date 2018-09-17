6da369d7c81e8abe82b620115ccd028de17bf019
Anton <antonoutkine@gmail.com>
2018-09-16 21:57:27 -0500

    Change backend url to https

--- src/main.js
+++ src/main.js
@@ -23,11 +23,12 @@ export const router = new Router(routerConfig)
 
 sync(store, router, { moduleName: 'router' })
 
+console.log(process.env.NODE_ENV)
 export const apolloClient = new ApolloClient({
   uri: `${
     process.env.NODE_ENV === 'development'
       ? 'http://127.0.0.1'
-      : 'http://ec2-18-216-16-19.us-east-2.compute.amazonaws.com'
+      : 'https://ec2-18-216-16-19.us-east-2.compute.amazonaws.com'
   }:8000/graphql/`,
   credentials: 'include',
 
