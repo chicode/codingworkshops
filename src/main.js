32b48ada81bddc71d288a7584f9b1a2ead48628f
Anton <antonoutkine@gmail.com>
2018-09-22 21:47:25 -0500

    Update backend url

--- src/main.js
+++ src/main.js
@@ -30,7 +30,7 @@ export const apolloClient = new ApolloClient({
   uri: `${
     process.env.NODE_ENV === 'development'
       ? 'http://127.0.0.1:8000'
-      : 'https://api.codingworkshops.org'
+      : 'https://codingworkshops.org/api'
   }/graphql/`,
   credentials: 'include',
 
