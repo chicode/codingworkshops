6eca53743e493074dacfb76b7d27db9545f920d4
Anton <antonoutkine@gmail.com>
2018-11-01 11:09:59 -0500

    Fix dependency issue

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   chainWebpack: (config) => {
+    // https://github.com/graphql/graphql-js/issues/1272
+    config.resolve.extensions.prepend('.mjs')
     // GraphQL Loader
     config.module
       .rule('graphql')
